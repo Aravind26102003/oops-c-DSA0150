22971b171a0841ef655e1c912109cef06e5643fe
Aravind26102003 <123789008+Aravind26102003@users.noreply.github.com>
2023-04-07 23:10:16 +0530

    Add files via upload

---
+++ ass4q3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+ using namespace std;
+
+ 
+int main()
+{
+	int x,n = 10;
+  for(x=1;x<=n;x++)
+  {
+  	cout<<x<<"\n";
+  }
+  
+  return 0;
+}
+
---
+++ ass4q5.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+ using namespace std;
+ int main()
+{
+   int a = 15, b = 20;
+   cout<<"max = "<<b;
+   cout<<"\n min ="<<a;
+   return 0;
+}
+

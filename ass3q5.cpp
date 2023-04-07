872b2f5eb6011bea5388b9f081c5ca39bfcbdad0
Aravind26102003 <123789008+Aravind26102003@users.noreply.github.com>
2023-04-07 23:00:57 +0530

    Add files via upload

---
+++ ass3q5.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+ using namespace std;
+ int main()
+{
+   int a = 15, b = 20;
+   cout<<"max = "<<b;
+   cout<<"min ="<<a;
+   return 0;
+}
+
---
+++ ass3q7.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+ 
+using namespace std;
+int main()
+{
+
+    cout<<" SIMATS ";
+    
+    cout<< "SCHOOL OF ENGINEERING ";
+     
+    return 0;
+}
+
